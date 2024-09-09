5e738537f973aca94f879cb2f458939e76346afd
Sachin Singh <108183252+Sachinsingh2002@users.noreply.github.com>
2024-09-09 14:31:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template,request
 import pickle
+import os
 
 app = Flask(__name__)
 
@@ -27,4 +28,5 @@ def main_function():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
